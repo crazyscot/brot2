5d9710bd6fa9a79867f02678e7adfdb2638d9a1e
Ross Younger <wry@onyx.virtual.impropriety.org.uk>
2010-11-20 10:43:54 +0000

    Kill off original (defunct) Plot interface

--- main.cpp
+++ main.cpp
@@ -36,7 +36,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.";
 #include <sys/stat.h>
 #include <unistd.h>
 
-#include "Plot.h"
 #include "Plot2.h"
 #include "Fractal.h"
 #include "palette.h"
