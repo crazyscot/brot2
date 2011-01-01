7c3530f89a102680fabe8ea011f4d0bc0e73d2dd
Ross Younger <wry@onyx.virtual.impropriety.org.uk>
2011-01-01 19:24:45 +0000

    ... and mandelbrot^4

--- Fractal.cpp
+++ Fractal.cpp
@@ -156,3 +156,63 @@ void Mandel3::plot_pixel(const int maxiter, fractal_point& out) const
 Mandel3 mandel3("Mandelbrot^3");
 #undef ITER3
 
+// --------------------------------------------------------------------
+
+class Mandel4 : public Mandelbrot {
+public:
+	Mandel4(std::string name_, fvalue xmin_=-3.0, fvalue xmax_=3.0, fvalue ymin_=-3.0, fvalue ymax_=3.0) : Mandelbrot(name_, xmin_, xmax_, ymin_, ymax_) {};
+	~Mandel4() {};
+
+	virtual void prepare_pixel(const cfpt coords, fractal_point& out) const;
+	virtual void plot_pixel(const int maxiter, fractal_point& iters_out) const;
+};
+
+
+void Mandel4::prepare_pixel(const cfpt coords, fractal_point& out) const
+{
+	//fvalue o_re = real(coords), o_im = imag(coords);
+
+	// TODO: Shortcuts?
+
+	// The first iteration is easy, 0^4 + origin = origin
+	out.origin = out.point = cfpt(coords);
+	out.iter = 1;
+	return;
+}
+
+void Mandel4::plot_pixel(const int maxiter, fractal_point& out) const
+{
+	// Speed notes:
+	// Don't use cfpt in the actual calculation - using straight doubles and
+	// doing the complex maths by hand is about 6x faster for me.
+	int iter;
+	fvalue o_re = real(out.origin), o_im = imag(out.origin),
+		   z_re = real(out.point), z_im = imag(out.point), re2, im2;
+
+#define ITER4() do { 								\
+		re2 = z_re * z_re;							\
+		im2 = z_im * z_im;							\
+		z_im = 4 * (re2*z_re*z_im - z_re*im2*z_im) + o_im;	\
+		z_re = re2*re2 - 6*re2*im2 + im2*im2 + o_re;		\
+} while (0)
+
+	for (iter=out.iter; iter<maxiter; iter++) {
+		ITER4();
+		if (re2 + im2 > 4.0) {
+			// Fractional escape count: See http://linas.org/art-gallery/escape/escape.html
+			ITER4(); ++iter;
+			ITER4(); ++iter;
+			out.iter = iter;
+			out.iterf = iter - log(log(re2 + im2)) / (2*consts.log2);
+			out.arg = atan2(z_im, z_re);
+			out.nomore = true;
+			return;
+		}
+	}
+	out.iter = iter;
+	out.point = cfpt(z_re,z_im);
+}
+
+Mandel4 mandel4("Mandelbrot^4");
+#undef ITER4
+
