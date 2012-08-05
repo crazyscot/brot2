fd5bb67b6037e6a72aca69bd4a0fb3651177f45d
Ross Younger <wry@onyx.virtual.impropriety.org.uk>
2012-08-05 21:07:06 +1200

    Create IJob and IJobEngine interfaces

---
+++ libbrot2/IJob.h
@@ -0,0 +1,36 @@
+/*
+    IJob.h: An abstract job to run on an IJobEngine
+    Copyright (C) 2012 Ross Younger
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ * Little is fixed about jobs at an abstract level. They are a bit like
+ * threads in that regard; indeed, they might well be run as threads
+ * on a multicore machine.
+ */
+
+#ifndef IJOB_H_
+#define IJOB_H_
+
+class IJobEngine;
+
+class IJob {
+public:
+	/* Called by the IJobEngine to run this job */
+	virtual void run(IJobEngine& engine) = 0;
+};
+
+#endif /* IJOB_H_ */
---
+++ libbrot2/IJobEngine.h
@@ -0,0 +1,67 @@
+/*
+    IJobEngine.h: Interface for job processing engines
+    Copyright (C) 2012 Ross Younger
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ * A Job Engine has a pile of jobs.
+ * They will be started in order but (of course) there is no guarantee of
+ * completion order or finishing order.
+ *
+ * The Engine takes a callback, which is called both for single
+ * jobs becoming complete, and for the whole thing.
+ *
+ * Some Engines may allow jobs to be added to (or even removed from) the
+ * queue while the engine is running.
+ */
+
+#ifndef IJOBENGINE_H_
+#define IJOBENGINE_H_
+
+#include <vector>
+#include "IJob.h"
+
+class IJobEngine;
+
+class IJobEngineCallback {
+public:
+	// A single Job has completed.
+	virtual void JobComplete(IJob*, IJobEngine&) = 0;
+
+	// Normal termination. All Jobs have finished.
+	virtual void JobEngineFinished(IJobEngine&) = 0;
+
+	// Somebody told us to stop. Called when all running Jobs have finished.
+	virtual void JobEngineStopped(IJobEngine&) = 0;
+};
+
+class IJobEngine {
+public:
+	/* NOTE: Implementations are expected to be passed an IJobEngineCallback
+	 * and call it suitably. */
+
+	/* Kicks off the engine.
+	 * Most implementations will do this asynchronously i.e. return
+	 * immediately from this call.
+	 * They are expected to take care of their own threading if so. */
+	virtual void start() = 0;
+
+	/* Stops the queue. Will result in a JobEngineStopped callback when
+	 * all running Jobs have stopped. */
+	virtual void stop() = 0;
+};
+
+#endif /* IJOBENGINE_H_ */
