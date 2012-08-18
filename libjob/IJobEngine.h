997a1bba2f28a1e71893efac1b2798786fa2236c
Ross Younger <onyx-commits@impropriety.org.uk>
2012-08-19 11:08:43 +1200

    Add locks around sigc slots to kill occasional (5%) segv.

--- libjob/IJobEngine.h
+++ libjob/IJobEngine.h
@@ -32,12 +32,38 @@
 #define IJOBENGINE_H_
 
 #include <sigc++/sigc++.h>
+#include <glibmm/thread.h>
 #include "IJob.h"
 
 namespace job {
 
 class IJobEngine {
 private:
+	/*
+	 * Beware!
+	 * http://answerpot.com/showthread.php?2326750-Threads+and+gtkmm says:
+	 *
+	 * 2. Unless special additional synchronization is employed, any
+	 * particular sigc::signal object should be regarded as 'owned' by the
+	 * thread which created it. Only that thread should connect slots with
+	 * respect to the signal object, and only that thread should emit() or
+	 * call operator()() on it.
+	 *
+	 * 3. Unless special additional synchronization is employed, any
+	 * sigc::connection object should be regarded as 'owned' by the thread
+	 * which created the slot and called the method which provided the
+	 * sigc::connection object. Only that thread should call
+	 * sigc::connection methods on the object.
+	 *
+	 * Also beware that signals are emitted WITH THE ENGINE LOCK HELD, which
+	 * might lead to all manner of interesting deadlocks if the signals do
+	 * anything nontrivial.
+	 *
+	 * In a complex multi-threaded environment it would be a good idea to use
+	 * a Glib::Dispatcher or similar method to asynchronously pass these signal
+	 * over to another thread to actually act on them.
+	 */
+	Glib::Mutex _sigLock; // Protects all signal slots
 	sigc::signal<void, IJob*> _signalJobDone;
 	sigc::signal<void, IJob*> _signalJobFailed;
 	sigc::signal<void> _signalFinished;
@@ -61,18 +87,22 @@ public:
 
 	/* Who should we tell when a job is done? */
 	sigc::connection connect_JobDone(const sigc::slot<void,IJob*>& slot) {
+		Glib::Mutex::Lock _auto(_sigLock);
 		return _signalJobDone.connect(slot);
 	};
 	/* Who should we tell when a job fails? */
 	sigc::connection connect_JobFailed(const sigc::slot<void,IJob*>& slot) {
+		Glib::Mutex::Lock _auto(_sigLock);
 		return _signalJobFailed.connect(slot);
 	};
 	/* Who should we tell when all jobs are complete? */
 	sigc::connection connect_Finished(const sigc::slot<void>& slot) {
+		Glib::Mutex::Lock _auto(_sigLock);
 		return _signalFinished.connect(slot);
 	};
 	/* Who should we tell when we've stopped? */
 	sigc::connection connect_Stopped(const sigc::slot<void>& slot) {
+		Glib::Mutex::Lock _auto(_sigLock);
 		return _signalStopped.connect(slot);
 	};
 
@@ -84,16 +114,20 @@ public:
 protected:
 	/* To be called by implementations when jobs complete or fail, as appropriate */
 	void emit_JobDone(IJob* job) {
+		Glib::Mutex::Lock _auto(_sigLock);
 		_signalJobDone(job);
 	}
 	void emit_JobFailed(IJob* job) {
+		Glib::Mutex::Lock _auto(_sigLock);
 		_signalJobFailed(job);
 	}
 	/* To be called by implementations when finished or stopped, as appropriate */
 	void emit_Finished() {
+		Glib::Mutex::Lock _auto(_sigLock);
 		_signalFinished.emit();
 	}
 	void emit_Stopped() {
+		Glib::Mutex::Lock _auto(_sigLock);
 		_signalStopped.emit();
 	}
 };
