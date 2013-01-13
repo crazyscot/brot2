a9ac1cf2c4a7b5f7d3ca898bda1293db352d45ad
Ross Younger <onyx-commits@impropriety.org.uk>
2013-01-13 16:36:52 +1300

    Now we can act upon the HUDOutlineText pref.

--- gtkui/HUD.cpp
+++ gtkui/HUD.cpp
@@ -127,20 +127,39 @@ void HUD::draw(Plot3::Plot3Plot* plot, const int rwidth, const int rheight)
 	// Make sure we fit.
 	const int YOFFSET = ypos * (rheight - compute_layout_height(lyt)) / 100;
 
-	// Now paint the text background.
-	Pango::LayoutIter iter = lyt->get_iter();
-	do {
-		Pango::Rectangle log = iter.get_line_logical_extents();
+	if (parent.prefs()->get(PREF(HUDOutlineText))) {
+		// Outline text effect
 		cr->save();
-		cr->set_source_rgb(bg.r, bg.g, bg.b);
-		// NB. there are PANGO_SCALE pango units to the device unit.
-		cr->rectangle(XOFFSET + log.get_x() / PANGO_SCALE,
-				YOFFSET + log.get_y() / PANGO_SCALE,
-				log.get_width() / PANGO_SCALE, log.get_height() / PANGO_SCALE);
-		cr->clip();
-		cr->paint_with_alpha(alpha);
+		cr->begin_new_path();
+		cr->move_to(XOFFSET,YOFFSET);
+		if (fontsize <= 13)
+			cr->set_line_width(1.0);
+		else
+			cr->set_line_width(2.0);
+		cr->set_operator(Cairo::Operator::OPERATOR_OVER);
+		cr->set_source_rgba(bg.r, bg.g, bg.b, alpha);
+		lyt->update_from_cairo_context(cr);
+		lyt->add_to_cairo_context(cr);
+		cr->stroke_preserve();
+		lyt->show_in_cairo_context(cr);
 		cr->restore();
-	} while (iter.next_line());
+		lyt->update_from_cairo_context(cr);
+	} else {
+		// Boring rectangular text background
+		Pango::LayoutIter iter = lyt->get_iter();
+		do {
+			Pango::Rectangle log = iter.get_line_logical_extents();
+			cr->save();
+			cr->set_source_rgb(bg.r, bg.g, bg.b);
+			// NB. there are PANGO_SCALE pango units to the device unit.
+			cr->rectangle(XOFFSET + log.get_x() / PANGO_SCALE,
+					YOFFSET + log.get_y() / PANGO_SCALE,
+					log.get_width() / PANGO_SCALE, log.get_height() / PANGO_SCALE);
+			cr->clip();
+			cr->paint_with_alpha(alpha);
+			cr->restore();
+		} while (iter.next_line());
+	}
 
 	// Finally, draw the text itself.
 	cr->move_to(XOFFSET,YOFFSET);
