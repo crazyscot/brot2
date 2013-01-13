d567819567298f1662e296971a290940e33f6709
Ross Younger <onyx-commits@impropriety.org.uk>
2013-01-13 17:29:13 +1300

    Recut zoom output: Below 10000, output as decimal Above 10000, output as 12.34x10<sup>56</sup> (needs Pango markup support) Move the 'x' (when not showing Zoom:) before the number (looks neater)

--- libbrot2/Plot3Plot.cpp
+++ libbrot2/Plot3Plot.cpp
@@ -121,18 +121,35 @@ string Plot3Plot::info_zoom(bool show_legend) const {
 	/* In support of LP#1039385 */
 	Fractal::Value z = zoom();
 	std::ostringstream rv;
+	if (show_legend)
+		rv << "Zoom: ";
+	else
+		rv << "x";
+
 	if (z > 10000.0) {
 		rv.precision(2);
 		rv << scientific;
+		rv << z;
 	} else {
 		rv.precision(4);
+		rv << z;
 	}
-	if (show_legend)
-		rv << "Zoom: ";
-	rv << z;
-	if (!show_legend)
-		rv << "x";
-	return rv.str();
+
+	string rs = rv.str();
+	if (z > 10000.0) {
+		// Messy...
+		size_t pos = rs.find("e");
+		if(pos != std::string::npos)
+			rs.replace(pos, 1, "x10<sup>");
+		pos = rs.find("+0"); // Pesky extra zeroes, can't see how to turn them off
+		if(pos != std::string::npos)
+			rs.replace(pos, 2, "");
+		pos = rs.find("+"); // This one shouldn't get triggered, but be paranoid
+		if(pos != std::string::npos)
+			rs.replace(pos, 1, "");
+		rs.append("</sup>");
+	}
+	return rs;
 }
 
 /* Starts a plot. The actual work happens in the background. */
