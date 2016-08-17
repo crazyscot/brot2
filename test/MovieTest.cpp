f231372d8a84ca61dfa423fc0b0adaddc0d2482b
Ross Younger <onyx-commits@impropriety.org.uk>
2016-08-17 21:04:34 +1200

    Add more tests - translation and translate&zoom

--- test/MovieTest.cpp
+++ test/MovieTest.cpp
@@ -139,8 +139,42 @@ TEST(Translate, Terminates1) { do_translate_case(0.1,0.1, 0.2, 0.2); }
 TEST(Translate, Terminates2) { do_translate_case(0.1,0.2, 0.2, 0.1); }
 TEST(Translate, Terminates3) { do_translate_case(0.2,0.1, 0.1, 0.2); }
 TEST(Translate, Terminates4) { do_translate_case(0.2,0.2, 0.1, 0.1); }
+TEST(Translate, Terminates5) { do_translate_case(0.3,0.2, 0.1, 0.1); }
+TEST(Translate, Terminates6) { do_translate_case(0.2,0.3, 0.1, 0.1); }
+TEST(Translate, Terminates7) { do_translate_case(0.3,0.2, 0.1, 0.0); }
+TEST(Translate, Terminates8) { do_translate_case(0.3,0.2, 0.0, 0.1); }
 
 TEST(Translate, NonSquareTerminates) {
 	// This is a deliberately contrived example
 	do_translate_case( 0.1, 0.1, 0.5, 0.5, 0.1, 0.5 );
+}
+
+// -----------------------------------------------------------------------------
+
+TEST(ZoomAndTranslate, IteratesCorrectly) {
+	// Start and end co-ords/size
+	Fractal::Point centre1(-3.0, 3.0);
+	Fractal::Point centre2(-1.03, 0.37);
+	Fractal::Point size1(6.0, 6.0);
+	Fractal::Point size2(0.375, 0.375);
+
+	// Working registers
+	Fractal::Point cc(centre1), ss(size1);
+
+	int i=0;
+	while (i<1000000) {
+		//std::cout << "Step " << i << " centre=" << cc << ", size=" << ss << std::endl;
+		bool active = Movie::MotionZoom(ss, size2, TEST_WIDTH, TEST_HEIGHT, TEST_SPEED, ss);
+		active |= Movie::MotionTranslate(cc, centre2, ss, TEST_WIDTH, TEST_HEIGHT, TEST_SPEED, cc);
+		if (!active)
+			break;
+		++i;
+	}
+	EXPECT_LT(i, 1000000);
+	//std::cout << "DONE " << i << " centre=" << cc << ", size=" << ss << std::endl;
+		/* Plumbing into movie panel works fine in move or zoom alone.
+		 * Both together doesn't go to the correct out point.
+		 * So simulating here, expect correct answer. */
+	EXPECT_EQ(cc, centre2);
+	EXPECT_EQ(ss, size2);
 }
