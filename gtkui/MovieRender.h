b176e0d8669b5ad424e93129736e0f74ae418921
Ross Younger <onyx-commits@impropriety.org.uk>
2016-08-18 16:17:36 +1200

    Move render progress interfaces out to their own header

--- gtkui/MovieRender.h
+++ gtkui/MovieRender.h
@@ -26,35 +26,12 @@
 #include "palette.h"
 #include "Registry.h"
 #include "ThreadPool.h"
-#include "IPlot3DataSink.h"
+#include "IRenderProgress.h"
 #include <vector>
 #include <set>
 #include <atomic>
 
 namespace Movie {
-
-class Renderer;
-class IRenderCompleteHandler {
-	public:
-		// Signals that the given movie job has been completed.
-		virtual void signal_completion(Renderer& job) = 0;
-		virtual ~IRenderCompleteHandler() {}
-};
-
-class IRenderProgressReporter : public Plot3::IPlot3DataSink {
-	public:
-		// Setup. Not mandatory, we do the best we can if not called.
-		virtual void set_chunks_count(int n) = 0;
-		// Call when outputting multiple frames at once. We record @n@ frames as being plotted.
-		virtual void frames_traversed(int n) = 0;
-
-		// We also inherit from  Plot3::IPlot3DataSink:
-		// virtual void chunk_done(Plot3::Plot3Chunk* job) = 0;
-		// virtual void pass_complete(std::string& msg, unsigned passes_plotted, unsigned maxiter, unsigned pixels_still_live, unsigned total_pixels) = 0;
-		// virtual void plot_complete() = 0; // One plot = one FRAME of the movie.
-
-		virtual ~IRenderProgressReporter() {}
-};
 
 struct RenderInstancePrivate {
 	const struct Movie::MovieInfo& movie;
---
+++ libbrot2/IRenderProgress.h
@@ -0,0 +1,51 @@
+/*
+    IRenderProgress.h: Movie progress reporter interface
+    Copyright (C) 2016 Ross Younger
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef IRENDERPROGRESSREPORTER_H
+#define IRENDERPROGRESSREPORTER_H
+
+#include "IPlot3DataSink.h"
+
+namespace Movie {
+
+class Renderer;
+class IRenderCompleteHandler {
+	public:
+		// Signals that the given movie job has been completed.
+		virtual void signal_completion(Renderer& job) = 0;
+		virtual ~IRenderCompleteHandler() {}
+};
+
+class IRenderProgressReporter : public Plot3::IPlot3DataSink {
+	public:
+		// Setup. Not mandatory, we do the best we can if not called.
+		virtual void set_chunks_count(int n) = 0;
+		// Call when outputting multiple frames at once. We record @n@ frames as being plotted.
+		virtual void frames_traversed(int n) = 0;
+
+		// We also inherit from  Plot3::IPlot3DataSink:
+		// virtual void chunk_done(Plot3::Plot3Chunk* job) = 0;
+		// virtual void pass_complete(std::string& msg, unsigned passes_plotted, unsigned maxiter, unsigned pixels_still_live, unsigned total_pixels) = 0;
+		// virtual void plot_complete() = 0; // One plot = one FRAME of the movie.
+
+		virtual ~IRenderProgressReporter() {}
+};
+
+}; // namespace Movie
+
+#endif // IRENDERPROGRESSREPORTER_H
